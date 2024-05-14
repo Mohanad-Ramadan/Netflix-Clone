897a2efbca6d440312b1f6376503eb42c94defd5
Hon_Dev <mohanadramadan.m1@gmail.com>
2024-05-14 03:48:46 +0300

    refactor: clean up the NetworkManager code and added connectivity feature to URLSession

--- Netflix Clone/Managers/NetworkManager.swift
+++ Netflix Clone/Managers/NetworkManager.swift
@@ -18,67 +18,28 @@ class NetworkManager {
         decoder.dateDecodingStrategy = .iso8601
     }
     
-    func getDataOf(_ endpoint: Endpoints) async throws -> [Media] {
-        let stringURL = Constants.createUrlWith(endpoint)
-        guard let url = URL(string: stringURL) else {throw APIError.invalidURL}
-        
-        let (data,response) = try await URLSession.shared.data(from: url)
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {throw APIError.invalidResponse}
-        
-        do {
-            let fetchedData = try self.decoder.decode(MediaResponse.self, from: data)
-            return fetchedData.results
-        } catch {throw APIError.invalidData}
-    }
     
-    func getMoreOf(genresId: String, unwantedGenresId: String = "", ofMediaType mediaType: String = "movie", page: Int = 1) async throws -> [Media] {
-        let stringURL = Constants.createMoreLikeURLWith(mediaType: mediaType, genresId: genresId, without: unwantedGenresId, page: page)
+    //MARK: - Main Fetch Methods
+    func getMedia(from stringURL: String) async throws -> [Media] {
         guard let url = URL(string: stringURL) else {throw APIError.invalidURL}
         
-        let (data,response) = try await URLSession.shared.data(from: url)
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {throw APIError.invalidResponse}
-        
-        do {
-            let fetchedData = try self.decoder.decode(MediaResponse.self, from: data)
-            return fetchedData.results
-        } catch {throw APIError.invalidData}
-    }
-    
-    func getImagesFor(mediaId id: Int, ofType mediaType: String) async throws -> MediaImage {
-        let stringURL = Constants.createImageURLWith(mediaType: mediaType, id: id)
-        guard let url = URL(string: stringURL) else {throw APIError.invalidURL}
-        
-        let (data,response) = try await URLSession.shared.data(from: url)
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {throw APIError.invalidResponse}
-        
-        do { return try self.decoder.decode(MediaImage.self, from: data) }
-        catch { throw APIError.invalidData }
-    }
-    
-    func getCastFor(mediaId id: Int, ofType mediaType: String) async throws -> Cast {
-        let stringURL = Constants.createCastURLWith(mediaType: mediaType, id: id)
-        guard let url = URL(string: stringURL) else {throw APIError.invalidURL}
-        
-        let (data,response) = try await URLSession.shared.data(from: url)
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {throw APIError.invalidResponse}
-        
-        do { return try self.decoder.decode(Cast.self, from: data) }
-        catch { throw APIError.invalidData }
-    }
-    
-    func getTrailersFor(mediaId id: Int, ofType mediaType: String) async throws -> Trailer {
-        let stringURL = Constants.createTrailersURLWith(mediaType: mediaType, id: id)
-        guard let url = URL(string: stringURL) else {throw APIError.invalidURL}
-        
-        let (data,response) = try await URLSession.shared.data(from: url)
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {throw APIError.invalidResponse}
-        
-        do { return try self.decoder.decode(Trailer.self, from: data) }
-        catch { throw APIError.invalidData }
-    }
+        let configuration = URLSession.shared.configuration
+        configuration.waitsForConnectivity = true
+        configuration.timeoutIntervalForResource = 30
 
-    func getDetailsFor<T: Codable>(mediaId id: Int, ofType mediaType: String) async throws -> T {
-        let stringURL = Constants.createDetailsURLWith(mediaType: mediaType, id: id)
+        let session = URLSession(configuration: configuration)
+        let (data,response) = try await session.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {throw APIError.invalidResponse}
+        
+        do {
+            let fetchedData = try self.decoder.decode(MediaResponse.self, from: data)
+            return fetchedData.results
+        } catch { throw APIError.invalidData }
+    }
+    
+    
+    func getData<T: Codable>(from stringURL: String) async throws -> T {
         guard let url = URL(string: stringURL) else {throw APIError.invalidURL}
         
         let (data,response) = try await URLSession.shared.data(from: url)
@@ -87,30 +48,57 @@ class NetworkManager {
         do { return try self.decoder.decode(T.self, from: data) }
         catch { throw APIError.invalidData }
     }
+    
+    
+    //MARK: - Spacific Methods
+    func getImagesFor(mediaId id: Int, ofType mediaType: String) async throws -> MediaImage {
+        let stringURL = Constants.createImageURLWith(mediaType: mediaType, id: id)
+        return try await getData(from: stringURL)
+    }
+    
+    func getCastFor(mediaId id: Int, ofType mediaType: String) async throws -> Cast {
+        let stringURL = Constants.createCastURLWith(mediaType: mediaType, id: id)
+        return try await getData(from: stringURL)
+    }
+    
+    func getTrailersFor(mediaId id: Int, ofType mediaType: String) async throws -> Trailer {
+        let stringURL = Constants.createTrailersURLWith(mediaType: mediaType, id: id)
+        return try await getData(from: stringURL)
+    }
+
+    func getDetailsFor<T: Codable>(mediaId id: Int, ofType mediaType: String) async throws -> T {
+        let stringURL = Constants.createDetailsURLWith(mediaType: mediaType, id: id)
+        return try await getData(from: stringURL)
+    }
 
     func getSeasonDetailsFor(seriesId: Int, seasonNumber: Int) async throws -> SeasonDetail {
         let stringURL = Constants.createSeasonURLWith(id: seriesId, seasonNumber: seasonNumber)
-        guard let url = URL(string: stringURL) else {throw APIError.invalidURL}
-        
-        let (data,response) = try await URLSession.shared.data(from: url)
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {throw APIError.invalidResponse}
-        
-        do { return try self.decoder.decode(SeasonDetail.self, from: data) }
-        catch {throw APIError.invalidData }
+        return try await getData(from: stringURL)
     }
+    
+    
+    // media fetch methods
+    func getDataOf(_ endpoint: Endpoints) async throws -> [Media] {
+        let stringURL = Constants.createUrlWith(endpoint)
+        return try await getMedia(from: stringURL)
+    }
+    
+    
+    func getMoreOf(
+        genresId: String,
+        unwantedGenresId: String = "",
+        ofMediaType mediaType: String = "movie",
+        page: Int = 1
+    ) async throws -> [Media] {
+        let stringURL = Constants.createMoreLikeURLWith(mediaType: mediaType, genresId: genresId, without: unwantedGenresId, page: page)
+        return try await getMedia(from: stringURL)
+    }
+    
     
     func getSearches(of query: String, page: Int = 1) async throws -> [Media] {
         guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {throw APIError.invalidURL}
         let stringURL = Constants.createSearchURLFor(query, page: page)
-        guard let url = URL(string: stringURL) else {throw APIError.invalidURL}
-        
-        let (data,response) = try await URLSession.shared.data(from: url)
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {throw APIError.invalidResponse}
-        
-        do {
-            let fetchedData = try self.decoder.decode(MediaResponse.self, from: data)
-            return fetchedData.results
-        } catch {throw APIError.invalidData}
+        return try await getMedia(from: stringURL)
     }
     
 }
